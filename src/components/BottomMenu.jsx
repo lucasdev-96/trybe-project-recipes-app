90c5c11d59a4624d856be3683df4bee16cbf9f1b
luanlsr <luanfswd@gmail.com>
2021-06-30 00:30:19 -0300

    feat: requisito 22, 23 e 24 finalizados

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -1,5 +1,6 @@
 // começando
 import React from 'react';
+import { Link } from 'react-router-dom';
 import mealIcon from '../images/mealIcon.svg';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
@@ -9,9 +10,16 @@ export default function BottomMenu() {
   return (
     <div data-testid="footer" className="footer">
       <ul className="icons-container">
-        <li><img data-testid="food-bottom-btn" src={ mealIcon } alt="" /></li>
-        <li><img data-testid="explore-bottom-btn" src={ exploreIcon } alt="" /></li>
-        <li><img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="" /></li>
+        <Link to="/comidas">
+          <li><img data-testid="food-bottom-btn" src={ mealIcon } alt="" /></li>
+        </Link>
+        <Link to="/explorar">
+          <li><img data-testid="explore-bottom-btn" src={ exploreIcon } alt="" /></li>
+        </Link>
+        <Link to="/bebidas">
+          <li><img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="" /></li>
+        </Link>
+
       </ul>
     </div>
   );
