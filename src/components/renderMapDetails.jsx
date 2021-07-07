f8715b0a89bca7dbad537783717149392e3047e0
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-07-07 02:00:15 -0300

    terminando requistios e estilizando a pag de details

--- src/components/renderMapDetails.jsx
+++ src/components/renderMapDetails.jsx
@@ -3,6 +3,7 @@ import { FaShareAlt } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import { MdFavorite } from 'react-icons/md';
 import RecipesContext from '../contexts/RecipesContext';
+import arrayIngredients from '../helpers/arrayIngredients';
 import '../styles/mapDetails.css';
 
 function RenderMapDetails({ path }) {
@@ -29,37 +30,58 @@ function RenderMapDetails({ path }) {
 
   return (
     <div className="FatherMap">
-      {api.map((value, index) => {
-        const ingredients = [`${value.strIngredient1}${value.strMeasure1}`];
-        return (
-          <section className="father" key={ index }>
-            <div className="fatherImg">
-              <img
-                className="img"
-                data-testid="recipe-photo"
-                src={ value[`str${keyMap}Thumb`] }
-                alt={ value[`str${keyMap}`] }
-              />
+      {api.map((valueApiKeys, index) => (
+        <section className="father" key={ index }>
+          <div className="fatherImg">
+            <img
+              className="img-fluid"
+              data-testid="recipe-photo"
+              src={ valueApiKeys[`str${keyMap}Thumb`] }
+              alt={ valueApiKeys[`str${keyMap}`] }
+            />
+          </div>
+          <section className="infos">
+            <div className="buttons">
+              <h1 data-testid="recipe-title">
+                {valueApiKeys[`str${keyMap}`]}
+              </h1>
+              <button className="effectBtn" type="button" data-testid="share-btn">
+                <FaShareAlt />
+              </button>
+              <button className="effectBtn" type="button" data-testid="favorite-btn">
+                <MdFavorite />
+              </button>
             </div>
-            <section className="infos">
-              <div className="buttons">
-                <h1 data-testid="recipe-title">
-                  {value[`str${keyMap}`]}
-                </h1>
-                <button className="effectBtn" type="button" data-testid="share-btn">
-                  <FaShareAlt />
-                </button>
-                <button className="effectBtn" type="button" data-testid="favorite-btn">
-                  <MdFavorite />
-                </button>
-              </div>
-              <p data-testid="recipe-category">{value[category]}</p>
-              <h2>Ingredients</h2>
-            </section>
+            <p data-testid="recipe-category">{valueApiKeys[category]}</p>
+            <h2 className="h2">Ingredients</h2>
+            <ul className="list-group">
+              {arrayIngredients(valueApiKeys).map((name, index2) => {
+                if (name === '  '
+                  || name === 'null null'
+                  || name === 'undefined undefined'
+                  || name === ' '
+                ) return null;
+                return (
+                  <li
+                    className="list-group-item list"
+                    key={ index2 }
+                    data-testid={ `${index2}-ingredient-name-and-measure` }
+                  >
+                    {name}
+                  </li>
+                );
+              })}
+            </ul>
+            <h2 className="h2">Instructions</h2>
+            <div className="container-lg">
+              <p data-testid="instructions" className="text-justify intructionsText">
+                {valueApiKeys.strInstructions}
+              </p>
+            </div>
           </section>
-        );
-      })}
-{/* 
+        </section>
+      ))}
+      {/*
 Os ingredientes devem possuir o atributo data-testid="${index}-ingredient-name-and-measure";
 O texto de instruções deve possuir o atributo data-testid="instructions";
 O vídeo, presente somente na tela de comidas, deve possuir o atributo data-testid="video";
