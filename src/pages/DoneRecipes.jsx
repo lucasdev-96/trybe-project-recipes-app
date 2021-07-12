e618c4b13df8467ac79cf12900fb75c26821b764
luanlsr <luanfswd@gmail.com>
2021-07-12 17:34:40 -0300

    tela de receitas feitas finalizadas

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,18 +1,120 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
-import DoneCards from '../components/DoneCards';
+import RecipesContext from '../contexts/RecipesContext';
+
+import shareIcon from '../images/shareIcon.svg';
 import '../styles/DoneRecipes.css';
 
+const copy = require('clipboard-copy');
+
+// import DoneCards from '../components/DoneCards';
+
 function DoneRecipes() {
+  const { doneRecipes } = useContext(RecipesContext);
+
+  console.log(doneRecipes);
+  const [hasLinkCopied, setHasLinkCopied] = useState(false);
+  const [filteredDoneRecipes, setfilteredDoneRecipes] = useState([...doneRecipes]);
+
+  const handleShareClick = (type, id) => {
+    copy(`http://localhost:3000/${type}s/${id}`);
+
+    setHasLinkCopied(true);
+
+    const messageDuration = 3000;
+    setTimeout(() => {
+      setHasLinkCopied(false);
+    }, messageDuration);
+  };
+
+  const handleFilterFood = () => {
+    setfilteredDoneRecipes(doneRecipes.filter((done) => done.type === 'comida'));
+  };
+
+  const handleFilterDrink = () => {
+    setfilteredDoneRecipes(doneRecipes.filter((done) => done.type === 'bebida'));
+  };
+
+  const handleFilterAll = () => {
+    setfilteredDoneRecipes(doneRecipes
+      .filter((done) => done.type === 'comida' || done.type === 'bebida'));
+  };
+
   return (
     <div>
       <Header title="Receitas Feitas" />
       <div className="done-container">
-        <button data-testid="filter-by-all-btn" type="button">All</button>
-        <button data-testid="filter-by-food-btn" type="button">Food</button>
-        <button data-testid="filter-by-drink-btn" type="button">Drink</button>
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+          onClick={ () => handleFilterAll() }
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          type="button"
+          onClick={ () => handleFilterFood() }
+        >
+          Food
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+          onClick={ () => handleFilterDrink() }
+        >
+          Drink
+        </button>
         <div className="done-cards">
-          <DoneCards />
+          {filteredDoneRecipes.map(({
+            name, image, category, alcoholicOrNot, doneDate, area, type, id, tags,
+          }, index) => (
+            <div key={ index }>
+              <Link to={ `/${type}s/${id}` }>
+                <img
+                  data-testid={ `${index}-horizontal-image` }
+                  src={ image }
+                  alt="receita"
+                  width="100px"
+                />
+              </Link>
+              <div className="doneCards-descriptions">
+                <h3 data-testid={ `${index}-horizontal-top-text` }>
+                  {area && `${area} - `}
+                  { alcoholicOrNot || category }
+                </h3>
+                <Link
+                  to={ `/${type}s/${id}` }
+                >
+                  <h2 data-testid={ `${index}-horizontal-name` }>{name}</h2>
+                </Link>
+                <h4 data-testid={ `${index}-horizontal-done-date` }>
+                  {doneDate}
+                </h4>
+                <button
+                  type="button"
+                  onClick={ () => handleShareClick(type, id) }
+                >
+                  <img
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    src={ shareIcon }
+                    alt="Compartilhar"
+                  />
+                  {hasLinkCopied && 'Link copiado!'}
+                </button>
+
+                {tags && tags.map((tagName, i) => (
+                  <div
+                    key={ i }
+                    data-testid={ `${index}-${tagName}-horizontal-tag` }
+                  >
+                    { tagName }
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
