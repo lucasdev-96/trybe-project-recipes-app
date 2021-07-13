caf25d83e07ec9709166c79f26bfdb6d8ca4be71
lucasdev-96 <lucasgodoidev96@gmail.com>
2021-07-13 19:13:28 -0300

    perfil stylezed

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,38 +1,72 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { MdEmail } from 'react-icons/md';
 import Header from '../components/Header';
 import BottomMenu from '../components/BottomMenu';
+import userImg from '../images/userImage.png';
+import favoriteRecipes from '../images/favoriteRecipes.svg';
+import checkedRecipes from '../images/checkedRecipes.svg';
+import '../styles/Perfil.css';
 
 function Perfil() {
   const user = JSON.parse(localStorage.getItem('user'));
   const { email } = user;
+  const history = useHistory();
 
   function onClick() {
     localStorage.clear();
   }
 
   return (
-    <div>
+    <>
       <Header title="Perfil" />
-      <img alt="profile" src="userImage.png" />
-      <h2 data-testid="profile-email">{ email }</h2>
-      <Link to="/receitas-favoritas">
-        <button type="button" data-testid="profile-favorite-btn">
-          Receitas Favoritas
-        </button>
-      </Link>
-      <Link to="/receitas-feitas">
-        <button type="button" data-testid="profile-done-btn">
-          Receitas Feitas
-        </button>
-      </Link>
-      <Link to="/">
-        <button type="button" data-testid="profile-logout-btn" onClick={ onClick }>
+      <div className="fatherPerfil">
+        <div className="perfilPicture">
+          <img alt="profile" src={ userImg } />
+          <div>
+            <MdEmail />
+            <h2 data-testid="profile-email">{ email }</h2>
+          </div>
+        </div>
+        <div className="FatherButtons-perfil">
+          <button
+            type="button"
+            className="btn-details-perfil"
+            data-testid="profile-favorite-btn"
+            onClick={ (() => history.push('/receitas-favoritas')) }
+          >
+            <img src={ favoriteRecipes } alt="favorite" />
+
+            Receitas
+            <br />
+            Favoritas
+          </button>
+          <button
+            className="btn-details-perfil"
+            type="button"
+            data-testid="profile-done-btn"
+            onClick={ (() => history.push('/receitas-feitas')) }
+          >
+            <img src={ checkedRecipes } alt="checked" />
+            Receitas
+            <br />
+            Feitas
+          </button>
+        </div>
+        <button
+          className="logout"
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => {
+            onClick();
+            history.push('/');
+          } }
+        >
           Sair
         </button>
-      </Link>
+      </div>
       <BottomMenu />
-    </div>
+    </>
   );
 }
 
