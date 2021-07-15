54882d23f14c2044827e6eb1a703605ecea99d76
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-07-15 15:56:43 -0300

    Updates

---
+++ precache-manifest.1aadf651b2c8ca620eb2133badc59ca7.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d8b394a4c2ce3e6d0abc27ba65f660",
+    "url": "/reecipes-app/index.html"
+  },
+  {
+    "revision": "0e565c2e4d5ef76ec3f1",
+    "url": "/reecipes-app/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "d3d3a2309083c8fccb7c",
+    "url": "/reecipes-app/static/css/main.0febcf00.chunk.css"
+  },
+  {
+    "revision": "0e565c2e4d5ef76ec3f1",
+    "url": "/reecipes-app/static/js/2.8c24a716.chunk.js"
+  },
+  {
+    "revision": "27a9b0dfffcee2b8d5a0788841e8cf31",
+    "url": "/reecipes-app/static/js/2.8c24a716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d3a2309083c8fccb7c",
+    "url": "/reecipes-app/static/js/main.8336614b.chunk.js"
+  },
+  {
+    "revision": "3a922f209861a09f3b97",
+    "url": "/reecipes-app/static/js/runtime-main.e22e6602.js"
+  },
+  {
+    "revision": "5285111ae6d4104874bc847e6f6f224a",
+    "url": "/reecipes-app/static/media/NotFound.5285111a.svg"
+  },
+  {
+    "revision": "c2094f653a7f4625877bb62e3e0e07a9",
+    "url": "/reecipes-app/static/media/Recipes.c2094f65.gif"
+  },
+  {
+    "revision": "a04e7963f65d8b505454e334fd3d2e65",
+    "url": "/reecipes-app/static/media/bebidas.a04e7963.jpg"
+  },
+  {
+    "revision": "3087e4f819bce236eda568e40e2ece78",
+    "url": "/reecipes-app/static/media/blackHeartIcon.3087e4f8.svg"
+  },
+  {
+    "revision": "79051bd2569f210a157958b1f3fdeddc",
+    "url": "/reecipes-app/static/media/checkIcon.79051bd2.svg"
+  },
+  {
+    "revision": "e7084f0939d1804daf5bcf0f298cddcd",
+    "url": "/reecipes-app/static/media/checkedRecipes.e7084f09.svg"
+  },
+  {
+    "revision": "5671d1c21bdfbec7fb8a0390daaf5717",
+    "url": "/reecipes-app/static/media/comidas.5671d1c2.jpg"
+  },
+  {
+    "revision": "5f73de66ffbce8ef30b38702204bdf54",
+    "url": "/reecipes-app/static/media/drinkIcon.5f73de66.svg"
+  },
+  {
+    "revision": "30fcbf81c8622c1c94d43921848e2c82",
+    "url": "/reecipes-app/static/media/drinks.30fcbf81.jpg"
+  },
+  {
+    "revision": "46ea74bf1f963e9c0f24e795a9c298d9",
+    "url": "/reecipes-app/static/media/exploreIcon.46ea74bf.svg"
+  },
+  {
+    "revision": "d8f51bea3b0490c00acb468d31a9906c",
+    "url": "/reecipes-app/static/media/favoriteRecipes.d8f51bea.svg"
+  },
+  {
+    "revision": "968b730d346d68f9189c7f2758a5d554",
+    "url": "/reecipes-app/static/media/ingredientes.968b730d.jpg"
+  },
+  {
+    "revision": "54d3af5588fa742acaae21850cfc479a",
+    "url": "/reecipes-app/static/media/localizacao.54d3af55.jpg"
+  },
+  {
+    "revision": "374debffe5050b45e492ab4daa094ffc",
+    "url": "/reecipes-app/static/media/mealIcon.374debff.svg"
+  },
+  {
+    "revision": "c5d7a2c05772846cd4149597fe10ee97",
+    "url": "/reecipes-app/static/media/playIcon.c5d7a2c0.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/reecipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "4a85aa1984d65004d129338964e6de88",
+    "url": "/reecipes-app/static/media/searchIcon-2.4a85aa19.svg"
+  },
+  {
+    "revision": "07d3fc27de505ef17d6e9a631205f8fd",
+    "url": "/reecipes-app/static/media/searchIcon.07d3fc27.svg"
+  },
+  {
+    "revision": "0d16489b1441ce1b913be229d2fe5ca3",
+    "url": "/reecipes-app/static/media/shareIcon.0d16489b.svg"
+  },
+  {
+    "revision": "bd5b3a0285d82e184bc9bb1a136dd346",
+    "url": "/reecipes-app/static/media/suprisedrink.bd5b3a02.jpg"
+  },
+  {
+    "revision": "3ddb90f0857aa82bb03b3d4698f0d0d1",
+    "url": "/reecipes-app/static/media/surpreenda.3ddb90f0.jpg"
+  },
+  {
+    "revision": "753f696e93c6e26bc48e5e1f2833b9b7",
+    "url": "/reecipes-app/static/media/whiteHeartIcon.753f696e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/recipes-app/precache-manifest.37e73f1a9ee30ff1bdf339279f3afb31.js"
+  "/reecipes-app/precache-manifest.1aadf651b2c8ca620eb2133badc59ca7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recipes-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reecipes-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
